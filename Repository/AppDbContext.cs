2eb5a0a5c9400d04a07759919c42586bf6377eea
Ece Ataman <114909014+eceataman@users.noreply.github.com>
2024-09-04 16:47:39 +0300

    migration yapıldı

--- Repository/AppDbContext.cs
+++ Repository/AppDbContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,7 @@ namespace App.Repositories
         public DbSet<Product> Products { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
             base.OnModelCreating(modelBuilder);
         }
     }
